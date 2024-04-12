085a7cfda883d58c38c59c246b8d779fac96879c
Vulpes231 <whykayshot@gmail.com>
2024-04-12 14:27:30 +0100

    tweak

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,31 +32,36 @@ export const blogArticles = [
   },
   {
     id: 4,
+    title: "xanax facts",
+    link: "https://www.healthline.com/health/drugs/xanax-dosage#dosage",
+  },
+  {
+    id: 5,
     title: "recommended way to take mushroom",
     link: "https://www.psilocybinsf.com/blog/what-is-lemon-tek",
   },
   {
-    id: 5,
+    id: 6,
     title: "why do we think time is an illusion while on a trip?",
     link: "https://www.psilocybinsf.com/blog/time-is-an-illusion-while-on-trip",
   },
   {
-    id: 6,
+    id: 7,
     title: "guidelines for being an effective trip sitter",
     link: "https://www.psilocybinsf.com/blog/guidelines-for-being-an-effective-trip-sitter",
   },
   {
-    id: 7,
+    id: 8,
     title: "5 must-read books about psychedelics",
     link: "https://www.psilocybinsf.com/blog/5-must-read-books-in-psychedelics",
   },
   {
-    id: 8,
+    id: 9,
     title: "the synergy between music and psychedelics",
     link: "https://www.psilocybinsf.com/blog/the-synergy-between-music-and-psychedelics",
   },
   {
-    id: 9,
+    id: 10,
     title: "5 must-watch documentaries in psychedelics",
     link: "https://www.psilocybinsf.com/blog/5-must-watch-documentaries-in-psychedelics",
   },
